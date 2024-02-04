667d217a869394998a332c598598c4aa30251d7e
Niraj Palecha <nirajftw@gmail.com>
2024-02-05 03:25:24 +0530

    solve kata/a_case_of_snakes

--- katas/a_case_of_snakes.go
+++ katas/a_case_of_snakes.go
@@ -5,15 +5,42 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strings"
 )
 
 func main() {
-
-	pattern := regexp.MustCompile(`/[A-Z]+/`)
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		input := scanner.Text()
-		fmt.Println(pattern.MatchString(input))
-
+		fmt.Println(regex(input))
 	}
+	// isPrefixStripped := false
+	// convertedString := ""
+	// 	for _, c := range input {
+	// 		chr := string(c)
+	// 		if !isPrefixStripped {
+	// 			_, err := strconv.Atoi(chr)
+	// 			if strings.ToUpper(chr) == chr && err != nil {
+	// 				isPrefixStripped = true
+	// 				convertedString = convertedString + strings.ToLower(chr)
+	// 			}
+	// 		} else {
+	// 			if strings.ToLower(chr) == chr {
+	// 				convertedString = convertedString + chr
+	// 			} else {
+	// 				convertedString = convertedString + "_" + strings.ToLower(chr)
+	// 			}
+	// 		}
+	// 	}
+	// 	fmt.Println(convertedString)
+	// }
+}
+
+func regex(input string) string {
+	re := regexp.MustCompile("[a-zA-Z][a-z0-9]*")
+	result := re.FindAllString(input, -1)
+	if len(result[0]) <= 3 {
+		result = result[1:]
+	}
+	return strings.ToLower(strings.Join(result, "_"))
 }
