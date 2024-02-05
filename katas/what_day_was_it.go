c8e0cf976b392cbeea81e7a22c1f7927d189850b
Niraj Palecha <nirajftw@gmail.com>
2024-02-06 02:42:33 +0530

    solve what_day_was_it

---
+++ katas/what_day_was_it.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		numDays, err := strconv.Atoi(input)
+		if err != nil {
+			log.Fatalf("error parsing input to int: %v", err)
+		}
+		var dayZero time.Time = time.Unix(0, 0)
+		result := dayZero.Add(time.Hour * 24 * time.Duration(numDays))
+		fmt.Println(result.Weekday())
+	}
+}
