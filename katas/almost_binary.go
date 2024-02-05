03496ee66d6e225236b04088fc4230b61939c0ea
Niraj Palecha <nirajftw@gmail.com>
2024-02-06 02:28:14 +0530

    solve almost_binary

---
+++ katas/almost_binary.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	// 0 == . | 1 == #
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		binary := ""
+		for _, c := range input {
+			switch c {
+			case '.':
+				binary += "0"
+			case '#':
+				binary += "1"
+			default:
+			}
+		}
+		decimal, err := strconv.ParseInt(binary, 2, 64)
+		if err != nil {
+			log.Fatalf("failed to convert to decimal: %v", err)
+		}
+		fmt.Println(decimal)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
