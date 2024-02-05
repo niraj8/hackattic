e1ade1edc9dd9ed7cb5eb1271909b8ece5185c13
Niraj Palecha <nirajftw@gmail.com>
2024-02-06 02:08:03 +0530

    solve yes_it_fizz

---
+++ katas/yes_it_fizz.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		splits := strings.Split(input, " ")
+		start, end := MustParseInt(splits[0]), MustParseInt(splits[1])
+		for i := start; i <= end; i++ {
+			switch {
+			case i%15 == 0:
+				fmt.Println("FizzBuzz")
+			case i%3 == 0:
+				fmt.Println("Fizz")
+			case i%5 == 0:
+				fmt.Println("Buzz")
+			default:
+				fmt.Println(i)
+			}
+		}
+	}
+}
+
+func MustParseInt(s string) int {
+	parsedInt, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return parsedInt
+}
